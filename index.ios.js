4b3ec893c27ee9d80f3485803bfe3caf070728f4
Robin Champsaur <robin.champsaur.dev@gmail.com>
2017-04-28 19:06:59 -0400

    First try

--- index.ios.js
+++ index.ios.js
@@ -5,12 +5,64 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry,StyleSheet,Text,View } from 'react-native';
+import { AppRegistry,StyleSheet,Text,View, Navigator } from 'react-native';
 import MapViewPins from './Code/iosCode/MapViewPins.js'
 import SignIn from './Code/iosCode/SignIn.js'
 import UserPage from './Code/iosCode/UserPage.js'
 
-export default class StraightPoop extends Component {
+import * as firebase from 'firebase';
+
+//Initiaizing firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDUZjvISVXl1Xl64PaDU2Zi0uRn6jdG9gA",
+  authDomain: "gofu-9c8fb.firebaseio.com",
+  databaseURL: "https://gofu-9c8fb.firebaseio.com/",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default class Navigate extends Component{
+
+  render() {
+    return (
+      <Navigator
+        style={{ flex: 1 }}
+        initialRoute={{ name: 'StraightPoop' }}
+        renderScene={this.renderScene.bind(this)}
+      />
+    )
+  }
+
+  renderScene(route, navigator) {
+    //Loads the main page
+    if (route.name == 'StraightPoop'){
+      return <StraightPoop navigator={navigator} firebaseApp={firebaseApp} />
+    }
+
+    if (route.name == 'UserPage'){
+      return <UserPage navigator={navigator} firebaseApp={firebaseApp} />
+    }
+
+  }
+
+}
+
+AppRegistry.registerComponent('StraightPoop', () => Navigate);
+
+class StraightPoop extends Component {
+
+  async componentWillMount(){
+    //Check if userData is stored on device else open Login
+    AsyncStorage.getItem('userData').then((user_data_json) => {
+      let user_data = JSON.parse(user_data_json);
+      if(user_data != null){
+        this.props.navigator.push({
+          name: 'UserPage'
+        });
+      }
+    });
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
@@ -25,7 +77,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
@@ -38,5 +89,3 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
-
-AppRegistry.registerComponent('StraightPoop', () => StraightPoop);
