3940c628bd56ebfbd4c91ad2dad4978f0a50007a
Robin Champsaur <robin.champsaur.dev@gmail.com>
2017-04-29 01:27:26 -0400

    Added pin implemantation to mapview

--- Code/iosCode/MapViewPins.js
+++ Code/iosCode/MapViewPins.js
@@ -2,28 +2,70 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
+var markers = {
+  latlng: { latitude: 37.78825, longitude: -122.4324 }
+}
+
+
 class MapViewPins extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      //states
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      markers: [{
+        key: 1,
+        title: 'hello',
+        latlng: {
+          latitude: 37.78825,
+          longitude: -122.4324
+        },
+      },
+      {
+        key: 2,
+        title: 'hello',
+        latlng: {
+          latitude: 37.78827,
+          longitude: -122.4226
+        },
+      }]
     }
   }
+
+
+  // onRegionChange={this.onRegionChange()}>
+
 
   render() {
     return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }} />
+          initialRegion={this.state.region}>
+
+          {this.state.markers.map(marker => (
+            <MapView.Marker
+              coordinate={marker.latlng}
+              title={marker.title}
+              key={marker.key}
+            //  description={marker.description}
+            />
+          ))}
+
+        </MapView>
       </View>
     )
+  }
+
+  onRegionChange(region) {
+    this.setState({
+      region
+    });
   }
 }
 
